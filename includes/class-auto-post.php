c40f8b7e17673af58a8b4e51ce1a99b9d1dc4bfe
obiPlabon <obiplabon@gmail.com>
2021-07-04 20:17:28 +0600

    Updated post delete and status update functions

--- includes/class-auto-post.php
+++ includes/class-auto-post.php
@@ -14,10 +14,42 @@ class Auto_Post {
 	const SUB_POSTS_META_KEY = '_archiv_sub_posts';
 
 	public function __construct() {
-		add_action( 'wp_insert_post', [ $this, 'create_posts' ], 10, 2 );
-		add_filter( 'display_post_states', [ $this, 'add_post_states' ], 10, 2 );
-		add_filter( 'the_title', [ $this, 'the_title' ], 10, 2 );
+		add_action( 'wp_trash_post', [ $this, 'on_trash_post' ] );
+		add_action( 'save_post', [ $this, 'on_create_post' ], 10, 2 );
+		add_filter( 'display_post_states', [ $this, 'add_post_states' ], 100, 2 );
 		add_filter( 'elementor/frontend/admin_bar/settings', [ $this, 'add_admin_bar_menu' ], 10, 2 );
+	}
+
+	public function on_trash_post( $post_id ) {
+		$post = get_post( $post_id );
+
+		if ( empty( $post ) || get_post_type( $post ) !== Post_Types::VIEWING_ROOM ) {
+			return false;
+		}
+
+		if ( ! empty( $post->post_parent ) ) {
+			return false;
+		}
+
+		$args = [
+			'post_type'              => Post_Types::VIEWING_ROOM,
+			'posts_per_page'         => -1,
+			'post_parent'            => $post_id,
+			'update_post_meta_cache' => false,
+			'update_post_term_cache' => false,
+		];
+
+		$query = new \WP_Query( $args );
+
+		if ( $query->have_posts() ) {
+			remove_action( 'wp_trash_post', [ $this, 'on_trash_post' ] );
+
+			foreach ( $query->posts as $post ) {
+				wp_trash_post( $post->ID );
+			}
+
+			add_action( 'wp_trash_post', [ $this, 'on_trash_post' ] );
+		}
 	}
 
 	public function add_admin_bar_menu( $settings ) {
@@ -36,7 +68,7 @@ class Auto_Post {
 		}
 		
 		$post_ids = [ $parent_post_id ];
-		$_ids     = get_post_meta( $parent_post_id, self::SUB_POSTS_META_KEY, true );
+		$_ids = self::get_sub_posts( $parent_post_id );
 
 		if ( ! empty( $_ids ) ) {
 			$post_ids = array_merge( $post_ids, $_ids );
@@ -51,32 +83,23 @@ class Auto_Post {
 			];
 			
 			$posts = get_posts( $args );
+
 			foreach ( $posts as $post ) {
 				$title     = wp_trim_words( esc_html( $post->post_title ), 2, '...' );
 				$sub_title = wp_trim_words( esc_html( $post->post_title ), 3, '' );
 
 				$children[] = [
-					'id'        => "elementor_edit_doc_{$post->ID}",
 					'title'     => $title,
+					'id'        => "elementor_edit_doc_{$post->ID}",
 					'sub_title' => ( $post->ID !== $parent_post_id ? $sub_title : $this->get_base_post() ),
 					'href'      => get_the_permalink( $post->ID ),
-				];	
+				];
 			}
 
 			$settings['elementor_edit_page']['children'] = array_merge( $settings['elementor_edit_page']['children'], $children );
 		}
 
 		return $settings;
-	}
-
-	public function the_title( $title, $post_id ) {
-		$post = get_post( $post_id );
-
-		if ( ! is_admin() || get_post_type( $post ) !== Post_Types::VIEWING_ROOM || ! empty( $post->post_parent ) ) {
-			return $title;
-		}
-
-		return $this->get_base_post();
 	}
 
 	public function add_post_states( $post_states, $post ) {
@@ -88,38 +111,39 @@ class Auto_Post {
 			return false;
 		}
 
-		$sub_post_ids = get_post_meta( $post->ID, self::SUB_POSTS_META_KEY, true );
-
-		if ( empty( $sub_post_ids ) ) {
-			return false;
-		}
-
-		$post_states['title_as_states'] = strip_tags( $post->post_title );
+		unset( $post_states['elementor'] );
+		
+		$post_states['archiv-base-post'] = $this->get_base_post();
 
 		return $post_states;
 	}
 
-	public function create_posts( $post_id, $post ) {
+	public function on_create_post( $post_id, $post ) {
 		if ( get_post_type( $post_id ) !== Post_Types::VIEWING_ROOM || wp_is_post_autosave( $post_id ) ) {
 			return false;
 		}
 
-		if ( empty( $post ) || 'auto-draft' === $post->post_status ) {
+		if ( empty( $post ) || 'auto-draft' === $post->post_status || $post->post_parent ) {
 			return false;
 		}
 
-		$sub_post_ids = get_post_meta( $post_id, self::SUB_POSTS_META_KEY, true );
+		$sub_posts = self::get_sub_posts( $post_id );
+		if ( ! empty( $sub_posts ) && count( $sub_posts ) > 0 ) {
+			foreach ( $sub_posts as $sub_post ) {
+				wp_update_post( [
+					'ID'          => $sub_post,
+					'post_status' => $post->post_status,
+				] );
+			}
 
-		if ( $post->post_parent || ! empty( $sub_post_ids ) ) {
-			return false;
+			return;
 		}
 
-		remove_action( 'wp_insert_post', [ $this, 'create_posts' ], 10 );
+		remove_action( 'save_post', [ $this, 'on_create_post' ] );
 
-		$sub_post_ids = [];
-
-		foreach ( $this->get_sub_posts() as $sub_post ) {
-			$sub_post_ids[] = wp_insert_post( [
+		$sub_posts = [];
+		foreach ( $this->get_sub_posts_title() as $sub_post ) {
+			$sub_posts[] = wp_insert_post( [
 				'post_title'     => $sub_post,
 				'post_author'    => $post->post_author,
 				'post_status'    => $post->post_status,
@@ -130,12 +154,12 @@ class Auto_Post {
 			] );
 		}
 
-		update_post_meta( $post_id, self::SUB_POSTS_META_KEY, $sub_post_ids );
+		self::update_sub_posts( $post_id, $sub_posts );
 
-		add_action( 'wp_insert_post', [ $this, 'create_posts' ], 10, 2 );
+		add_action( 'save_post', [ $this, 'on_create_post' ], 10, 2 );
 	}
 
-	protected function get_sub_posts() {
+	protected function get_sub_posts_title() {
 		return [
 			'IMMERSION',
 			'LIST OF WORKS',
@@ -144,5 +168,22 @@ class Auto_Post {
 
 	protected function get_base_post() {
 		return 'ACADEMIC';
+	}
+
+	public static function get_sub_posts( $post_id ) {
+		return get_post_meta( $post_id, self::SUB_POSTS_META_KEY, true );
+	}
+
+	public static function has_sub_posts( $post_id ) {
+		$sub_posts = self::get_sub_posts( $post_id );
+		return ( ! empty( $sub_posts ) && count( $sub_posts ) > 1 );
+	}
+
+	public static function update_sub_posts( $post_id, $sub_posts = [] ) {
+		return update_post_meta( $post_id, self::SUB_POSTS_META_KEY, $sub_posts );
+	}
+
+	public static function delete_sub_posts( $post_id ) {
+		return delete_post_meta( $post_id, self::SUB_POSTS_META_KEY );
 	}
 }
