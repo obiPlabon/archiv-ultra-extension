40cd75d4a09ef14d2b4fef5501b5b2cb8c235ae7
obiPlabon <obiplabon@gmail.com>
2021-08-07 23:04:49 +0600

    Added styles

--- includes/class-widget-factory.php
+++ includes/class-widget-factory.php
@@ -11,6 +11,7 @@ defined( 'ABSPATH' ) || die();
 
 use Elementor\Widget_WordPress;
 use Elementor\Controls_Manager;
+use Elementor\Group_Control_Typography;
 
 class Widget_Factory extends Widget_WordPress {
 
@@ -30,21 +31,80 @@ class Widget_Factory extends Widget_WordPress {
 		$this->add_control(
 			'wp',
 			[
-				'label'       => __( 'Form', 'elementor' ),
+				'label'       => __( 'Form', 'archiv' ),
 				'type'        => Controls_Manager::WP_WIDGET,
 				'widget'      => $this->get_name(),
 				'id_base'     => $this->get_widget_instance()->id_base,
 			]
 		);
 
-		$this->start_controls_section(
-			'_section_nav_style',
-			[
-				'label' => __( 'Nav Style', 'plugin-name' ),
-                'tab' => Controls_Manager::TAB_STYLE,
-			]
-		);
+		if ( current_user_can( 'manage_options' ) || ! is_user_logged_in() ) {
 
-		$this->end_controls_section();
+			$this->start_controls_section(
+				'_section_menu_style',
+				[
+					'label' => __( 'Style', 'archiv' ),
+					'tab' => Controls_Manager::TAB_STYLE,
+				]
+			);
+
+			$this->add_control(
+				'_archiv_menu_padding',
+				[
+					'label' => __( 'Padding', 'archiv' ),
+					'type' => Controls_Manager::DIMENSIONS,
+					'size_units' => [ 'px', 'em', '%', 'rem' ],
+					'selectors' => [
+						'{{WRAPPER}} .archiv-menu .archiv-menu__item-link' => 'padding: {{TOP}}{{UNIT}} {{RIGHT}}{{UNIT}} {{BOTTOM}}{{UNIT}} {{LEFT}}{{UNIT}};',
+					],
+				]
+			);
+
+			$this->add_group_control(
+				Group_Control_Typography::get_type(),
+				[
+					'name'     => '_archiv_menu_typography',
+					'label'    => __( 'Typography', 'archiv' ),
+					'selector' => '{{WRAPPER}} .archiv-menu .archiv-menu__item-link',
+				]
+			);
+
+			$this->add_control(
+				'_archiv_menu_color',
+				[
+					'label' => __( 'Link Color', 'plugin-domain' ),
+					'type' => Controls_Manager::COLOR,
+					'selectors' => [
+						'{{WRAPPER}} .archiv-menu .archiv-menu__item-link' => 'color: {{VALUE}}',
+						'{{WRAPPER}} .archiv-menu .archiv-menu__item::after' => 'color: {{VALUE}}',
+					],
+				]
+			);
+
+			$this->add_control(
+				'_archiv_menu_color_hover',
+				[
+					'label' => __( 'Link Hover Color', 'plugin-domain' ),
+					'type' => Controls_Manager::COLOR,
+					'selectors' => [
+						'{{WRAPPER}} .archiv-menu .archiv-menu__item-link:hover, {{WRAPPER}} .archiv-menu .archiv-menu__item-link:focus' => 'color: {{VALUE}}',
+						'{{WRAPPER}} .archiv-menu .archiv-menu__item-link.archiv--is-active:hover, {{WRAPPER}} .archiv-menu .archiv-menu__item-link.archiv--is-active:focus' => 'color: {{VALUE}}',
+					],
+				]
+			);
+
+			$this->add_control(
+				'_archiv_menu_color_active',
+				[
+					'label' => __( 'Link Active Color', 'plugin-domain' ),
+					'type' => Controls_Manager::COLOR,
+					'selectors' => [
+						'{{WRAPPER}} .archiv-menu .archiv-menu__item-link.archiv--is-active' => 'color: {{VALUE}}',
+					],
+				]
+			);
+
+			$this->end_controls_section();
+		}
 	}
 }
